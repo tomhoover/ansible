62ed9976636976fe8ff05d6701beda14bbdb3472
Tom Hoover <tom@hisword.net>
2019-12-13 06:50:13 -0600

    add edit-all-roles.sh

---
+++ edit-all-roles.sh
@@ -0,0 +1,11 @@
+vi $(grep -rl '^ ' roles |grep -v 'tests/test.yml' |grep -v 'meta/main.yml' |grep -v 'README.md' |grep -v '.travis.yml' |grep -v 'smb.conf' |sort)
+#vi group_vars/*
+vi host_vars/*
+echo ""
+cat site.yml
+echo ""
+ls -l *.yml |grep -v site.yml |grep -v test.yml |grep -v uptime.yml
+echo ""
+ls -alR roles/*/files/*
+echo ""
+ls -alR roles/*/templates/*
