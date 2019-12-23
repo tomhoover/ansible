dd9c7c63cdb63cce9d70c1c090153c74fe73f397
Tom Hoover <tom@hisword.net>
2019-12-22 22:01:54 -0600

    revise edit-all-roles.sh to allow command line arguments

--- edit-all-roles.sh
+++ edit-all-roles.sh
@@ -1,6 +1,9 @@
-vi $(grep -rl '^ ' roles |grep -v 'tests/test.yml' |grep -v 'meta/main.yml' |grep -v 'README.md' |grep -v '.travis.yml' |grep -v 'smb.conf' |sort)
+vi $(grep -rl '^ ' roles |grep -v 'tests/test.yml' |grep -v 'meta/main.yml' |grep -v 'README.md' |grep -v '.travis.yml' |grep -v 'smb.conf' |sort) "$@"
+
 #vi group_vars/*
-vi host_vars/*
+#vi host_vars/*
+echo ""
+cat host_vars/*
 echo ""
 cat site.yml
 echo ""
