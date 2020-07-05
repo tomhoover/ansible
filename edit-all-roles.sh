510e375a1eab20ab97e64dfa08654d21c3ee3bd2
Tom Hoover <tom@hisword.net>
2020-07-04 21:43:41 -0500

    remove geerlingguy.postfix

--- edit-all-roles.sh
+++ edit-all-roles.sh
@@ -5,7 +5,7 @@
 # set -o pipefail: cause a pipeline to fail, if any command within it fails
 set -eu -o pipefail
 
-cd ~/.ansible && vi $(grep -rl '^ ' roles |grep -v 'tests/test.yml' |grep -v 'meta/main.yml' |grep -v 'README.md' |grep -v '.travis.yml' |grep -v 'smb.conf' |sort) "$@"
+cd ~/.ansible && vi $(grep -rl '^ ' roles |grep -v 'geerlingguy' |grep -v 'tests/test.yml' |grep -v 'meta/main.yml' |grep -v 'README.md' |grep -v '.travis.yml' |grep -v 'smb.conf' |sort) "$@"
 
 echo ""
 cat host_vars/*
