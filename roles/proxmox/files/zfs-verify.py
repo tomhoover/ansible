032a087d8ea141faa9bd19542c21810b8debdd4a
Tom Hoover <tom@hisword.net>
2020-08-13 12:39:44 -0500

    remove ariel-wl, add freenas

--- roles/proxmox/files/zfs-verify.py
+++ roles/proxmox/files/zfs-verify.py
@@ -11,7 +11,7 @@ MAX_SNAPSHOT_AGE = 24   # in hours
 
 ignored_filesystems =   { "freenas": "",
                           "pvhost1": "external external/backups external/backups/dump external/backups/freenas external/backups/hass external/backups/pvhost1 external/backups/pvhost1/external external/backups/pvhost2 external/backups/pvhost3 external/backups/z_old external/backups/z_old/** external/bethel-image external/pve-backups external/pve-templates", 
-                          "pvhost2": "backups backups/ariel-wl backups/arq backups/dump backups/duplicacy_backups backups/external backups/pvhost1 backups/pvhost1/external backups/pvhost2 backups/pvhost3 backups/pvhost3/pool backups/rsync backups/tar backups/unraid backups/unraid/** backups/z_old backups/z_old/** d14 d14/14 d15 d15/15 d16 d16/16 d17 d17/17 d18 d18/18 d19 d19/19 d20 d20/20 p1 p1/1 p25 p25/25", 
+                          "pvhost2": "backups backups/arq backups/dump backups/duplicacy_backups backups/external backups/freenas backups/pvhost1 backups/pvhost1/external backups/pvhost2 backups/pvhost3 backups/pvhost3/pool backups/rsync backups/tar backups/unraid backups/unraid/** backups/z_old backups/z_old/** d14 d14/14 d15 d15/15 d16 d16/16 d17 d17/17 d18 d18/18 d19 d19/19 d20 d20/20 p1 p1/1 p25 p25/25", 
                           "pvhost3": "external external/backups external/backups/dump external/backups/freenas external/backups/hass external/backups/pvhost1 external/backups/pvhost1/external external/backups/pvhost2 external/backups/pvhost3 external/backups/z_old external/backups/z_old/** external/bethel-image external/pve-backups external/pve-templates" }
 
 def filesystems():                  # list all zfs filesystems
